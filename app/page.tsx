71fca5140e77fe342610490eb894c78209e3a847
Daffa Hanifisyafiq <dandroid36@gmail.com>
2026-04-10 20:35:59 +0700

    feat: add animations and improve layout on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,102 @@
-import React from 'react'
-import SplashWrapper from './components/SplashWrapper'
+'use client'
+
 import Image from 'next/image'
+import { motion, MotionProps } from 'framer-motion'
+import SplashWrapper from './components/SplashWrapper'
 
 const Home = () => {
+  const smoothWind = (
+    duration: number,
+    xRange: number,
+    rRange: number,
+    delay: number = 0
+  ): MotionProps => ({
+    animate: {
+      x: [0, xRange, 0, -xRange, 0],
+      rotate: [0, rRange, 0, -rRange, 0],
+      y: [0, -3, 0, 3, 0]
+    },
+    transition: {
+      duration: duration,
+      repeat: Infinity,
+      ease: [0.445, 0.05, 0.55, 0.95],
+      delay: delay
+    }
+  })
+
   return (
-    <div className='bg-1 relative min-h-full w-full bg-cover bg-center bg-no-repeat'>
-      <Image
-        src='/images/black-tree-left.webp'
-        alt='black-tree-left'
-        width={300}
-        height={300}
-        className='absolute top-0 left-0'
-      />
-      <Image
-        src='/images/black-tree-right.webp'
-        alt='black-tree-right'
-        width={300}
-        height={300}
-        className='absolute top-0 right-0'
-      />
+    <main className='bg-1 shadow-custom relative min-h-screen w-full overflow-hidden bg-cover bg-center bg-no-repeat'>
+      <motion.div
+        {...smoothWind(12, 8, 3, 0)}
+        className='absolute -top-8 -left-8 z-10'
+      >
+        <Image
+          src='/images/black-tree-left.webp'
+          alt='btl'
+          width={300}
+          height={300}
+          priority
+        />
+      </motion.div>
+
+      <motion.div
+        {...smoothWind(14, -8, -3, 1)}
+        className='absolute -top-8 -right-8 z-10'
+      >
+        <Image
+          src='/images/black-tree-right.webp'
+          alt='btr'
+          width={300}
+          height={300}
+          priority
+        />
+      </motion.div>
+
+      <motion.div
+        {...smoothWind(10, 5, 4, 0.5)}
+        className='absolute -bottom-6 -left-6 z-20 origin-bottom-left'
+      >
+        <Image
+          src='/images/flower-left.webp'
+          alt='fl'
+          width={250}
+          height={250}
+        />
+      </motion.div>
+
+      <motion.div
+        {...smoothWind(11, -5, -4, 1.5)}
+        className='absolute -right-6 -bottom-6 z-20 origin-bottom-right'
+      >
+        <Image
+          src='/images/flower-right.webp'
+          alt='fr'
+          width={250}
+          height={250}
+        />
+      </motion.div>
+
       <Image
         src='/images/tree-left.webp'
-        alt='tree-left'
+        alt='tl'
         width={120}
         height={500}
-        className='absolute top-48 left-0 overflow-hidden'
+        className='absolute top-48 left-0 z-10 opacity-80'
       />
       <Image
         src='/images/tree-right.webp'
-        alt='tree-right'
+        alt='tr'
         width={120}
         height={500}
-        className='absolute top-48 right-0 overflow-hidden'
+        className='absolute top-48 right-0 z-10 opacity-80'
       />
-      <Image
-        src='/images/flower-left.webp'
-        alt='flower-left'
-        width={250}
-        height={250}
-        className='absolute bottom-0 left-0 overflow-hidden'
-      />
-      <Image
-        src='/images/flower-right.webp'
-        alt='flower-right'
-        width={250}
-        height={250}
-        className='absolute right-0 bottom-0 overflow-hidden'
-      />
-      <SplashWrapper />
-    </div>
+
+      <div className='relative z-30 flex h-screen w-full items-center justify-center'>
+        <div className='h-full w-full max-w-md'>
+          <SplashWrapper />
+        </div>
+      </div>
+    </main>
   )
 }
 
