13873fcbf2510edb04b2823942ea3e1d71e605e2
Daffa Hanifisyafiq <dandroid36@gmail.com>
2026-05-01 15:51:22 +0700

    chore: load firebase config from environment variables

--- app/config/firebase.ts
+++ app/config/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore, Firestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB6Fc8xv-_DCEc-4C33-bXCnOl-pNa6Qd4',
-  authDomain: 'wedding-invitation-nuril-daffa.firebaseapp.com',
-  projectId: 'wedding-invitation-nuril-daffa',
-  storageBucket: 'wedding-invitation-nuril-daffa.firebasestorage.app',
-  messagingSenderId: '697009348736',
-  appId: '1:697009348736:web:0e47792236b1db92b1c665'
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
 export const db: Firestore = getFirestore(app)
